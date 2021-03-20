171edb8d966d8063679e81af96495a2de16c89c7
AbdelAbdel <45332749+abdelabde@users.noreply.github.com>
2021-03-20 17:56:49 +0100

    Update __manifest__.py

--- amount_to_text/__manifest__.py
+++ amount_to_text/__manifest__.py
@@ -1,12 +1,11 @@
 
 {
     'name': 'Number to Amount',
-    'version': '1.1',
+    'version': '13.0',
     'summary': 'In any language convert number to amount for any currency',
     'description': "In any language convert number to amount for any currency",
     'depends': [
         'base',
-        'purchase'
     ],
     'demo': [
               ],
